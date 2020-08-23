e955f7851691d6048028fbad3443a2e59aab55dc
ms0223900 <ms0223900@gmail.com>
2020-08-23 20:57:07 +0800

    feat: update build sample

---
+++ build/precache-manifest.5755f203224810393119216c323566c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfaa4609cb17a3377677cbdfec7ef2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6028793973be660b7dee",
+    "url": "/static/js/2.47246c2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.47246c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e252e2434f298574d9",
+    "url": "/static/js/main.d0ecde05.chunk.js"
+  },
+  {
+    "revision": "6319c825c3b2865b60b3",
+    "url": "/static/js/runtime-main.8eb586b3.js"
+  }
+]);
