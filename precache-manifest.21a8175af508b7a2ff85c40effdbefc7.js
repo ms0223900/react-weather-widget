c2faefdd0b95b470dd5b3f2651ba2d4dbf84ecea
ms0223900 <ms0223900@gmail.com>
2020-08-23 21:07:45 +0800

    Updates

---
+++ precache-manifest.21a8175af508b7a2ff85c40effdbefc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dde94736eb7239f3c7f79516579f8e",
+    "url": "/react-weather-widget/index.html"
+  },
+  {
+    "revision": "6028793973be660b7dee",
+    "url": "/react-weather-widget/static/js/2.47246c2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-weather-widget/static/js/2.47246c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf8644182bd7c351b7d",
+    "url": "/react-weather-widget/static/js/main.9b5c8eef.chunk.js"
+  },
+  {
+    "revision": "61efa1f67ae3a394b828",
+    "url": "/react-weather-widget/static/js/runtime-main.0235c3b0.js"
+  }
+]);
